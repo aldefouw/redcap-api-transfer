c500c7087d326fce2499b2b72951289a6ea0c3df
Adam De Fouw <aldefouw@medicine.wisc.edu>
2017-10-24 16:09:04 -0500

    Remove unneeded methods and classes [RED-520]

--- library/reporting.rb
+++ library/reporting.rb
@@ -5,7 +5,6 @@ class Reporting
 
     create_errors_folder
     create_logs_folder
-    #make_errors_csv
 
     @error_log = Logger.new(errors_log_path)
     @info_log = Logger.new(info_log_path)
@@ -29,10 +28,6 @@ class Reporting
     "#{@base_dir}/logs/info.log"
   end
 
-  # def errors_csv_path
-  #   "#{@base_dir}/errors/errors.csv"
-  # end
-
   def info_output(text)
     puts text.green
     @info_log.info(text)
@@ -42,32 +37,5 @@ class Reporting
     puts text.red
     @error_log.error(text)
   end
-
-  # def make_errors_csv
-  #   if @project.longitudinal
-  #     CSV.open(errors_csv_path, "wb") { |csv| csv << ["record_id", "redcap_event_name", "instrument", "field", "case_report_form_value", "export_form_value", "url"] }
-  #   else
-  #     CSV.open(errors_csv_path, "wb") { |csv| csv << ["record_id", "instrument", "field", "case_report_form_value", "export_form_value", "url"] }
-  #   end
-  # end
-
-  # def add_error_to_file(record:, instrument:, field:, case_report_value:, export_value:, url:)
-  #   CSV.open(errors_csv_path, "a+") do |csv|
-  #     csv << csv_error_row(record: record,
-  #                          instrument: instrument,
-  #                          field: field,
-  #                          case_report_value: case_report_value,
-  #                          export_value: export_value,
-  #                          url:  url)
-  #   end
-  # end
-  #
-  # def csv_error_row(record:, instrument:, field:, case_report_value:, export_value:, url:)
-  #   if @project.longitudinal
-  #     [record[0], record["redcap_event_name"], instrument, field, case_report_value, export_value, url]
-  #   else
-  #     [record[0], instrument, field, case_report_value, export_value, url]
-  #   end
-  # end
 
 end
