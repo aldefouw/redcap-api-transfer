2e316295c60f0096d8273fe1e10a979399d44f0e
Adam De Fouw <aldefouw@medicine.wisc.edu>
2020-07-22 15:39:02 -0500

    Create project folder for errors and info [RED-1917]

--- app/transfer_app/library/reporting.rb
+++ app/transfer_app/library/reporting.rb
@@ -2,16 +2,23 @@ class Reporting
 
   def initialize(options)
     @base_dir = options[:base_dir]
+    @project_name = options[:config].project_name
     @error_log = Logger.new(errors_log_path)
     @info_log = Logger.new(info_log_path)
-    @project_name = options[:config].project_name
+  end
+
+  def create_logs_folders
+    Dir.chdir("#{@base_dir}/logs/")
+    Dir.mkdir(@project_name) unless Dir.exist?("#{@base_dir}/logs/#{@project_name}/")
   end
 
   def errors_log_path
+    create_logs_folders
     "#{@base_dir}/logs/#{@project_name}/errors.log"
   end
 
   def info_log_path
+    create_logs_folders
     "#{@base_dir}/logs/#{@project_name}/info.log"
   end
 
