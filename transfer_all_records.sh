cdf54bce97a7dc7babfd5c60e036acf6c99c80a8
Adam De Fouw <aldefouw@medicine.wisc.edu>
2020-07-24 10:15:50 -0500

    Remove TTY from the run commands [RED-1917]

--- transfer_all_records.sh
+++ transfer_all_records.sh
@@ -3,5 +3,5 @@
 if [ "$#" -ne 1 ]; then
     echo "Enter the name of the project to transfer as a single argument. Project names are specified in $(pwd)/config/config.yml."
 else
-  docker run -it -v="$(pwd)/config:/app/config" -v="$(pwd)/logs:/app/logs" redcap-api-file-transfer:latest ruby transfer_all_records.rb $1
+  docker run -v="$(pwd)/config:/app/config" -v="$(pwd)/logs:/app/logs" redcap-api-file-transfer:latest ruby transfer_all_records.rb $1
 fi
--- transfer_single_record.sh
+++ transfer_single_record.sh
@@ -3,5 +3,5 @@
 if [ "$#" -ne 2 ]; then
     echo "Enter the name of the project to transfer as the first argument and the record ID as the second. Project names are specified in $(pwd)/config/config.yml."
 else
-  docker run -it -v="$(pwd)/config:/app/config" -v="$(pwd)/logs:/app/logs" redcap-api-file-transfer:latest ruby transfer_single_record.rb $1 $2
+  docker run -v="$(pwd)/config:/app/config" -v="$(pwd)/logs:/app/logs" redcap-api-file-transfer:latest ruby transfer_single_record.rb $1 $2
 fi
